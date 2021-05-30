652c0f0abaef6a78bfe9f2ee3301ddb4520fdd75
Natavar Prajapati <prajapatinatavar21197@gmail.com>
2021-05-30 11:26:46 +0530

    Initial commit

---
+++ app/src/main/java/com/indialone/constraintlayoutproject_loginscreen/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.indialone.constraintlayoutproject_loginscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
